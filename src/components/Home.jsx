917a89f03b5b14146b198c7251ca8e1195ce71b0
karnking <karankotai9183@gmail.com>
2023-12-02 19:21:44 +0530

    navbar fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     const navigate = useNavigate()
     return (
         <>
-            <img alt='homeImg' className='z-0 fixed h-[100vh] w-[100vw] opacity-50' src={HomeImg} />
-            <div className='flex items-center h-[100vh] justify-center'>
+            <img alt='homeImg' className='z-0 absolute h-[91vh] w-[100vw] opacity-50' src={HomeImg} />
+            <div className='flex items-center h-[65vh] overflow-hidden justify-center'>
                 <button className='z-2 text-lg relative border-2 border-black bg-white text-black p-3 uppercase font-semibold rounded-md' onClick={()=>navigate('/favourites')}>Discover Packages ➡️</button>
             </div>
         </>
